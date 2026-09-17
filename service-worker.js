9f95761330231ebcc09dda0b320460a1caa1f5dd
TubaVogel <vogel.tuba@gmail.com>
2026-09-17 18:49:09 +0200

    Lesebibliothek als Offline-Web-App veröffentlichen

---
+++ service-worker.js
@@ -0,0 +1,12 @@
+const CACHE = "lesebibliothek-v1";
+const APP = ["./", "./index.html", "./manifest.webmanifest", "./icon-180.png", "./icon-512.png"];
+self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(APP))));
+self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))));
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request).then(response => {
+    const copy = response.clone();
+    caches.open(CACHE).then(cache => cache.put(event.request, copy));
+    return response;
+  }).catch(() => caches.match("./index.html"))));
+});
