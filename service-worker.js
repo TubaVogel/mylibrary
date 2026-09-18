25142f3607f801e53144243484664b71de824ec1
TubaVogel <vogel.tuba@gmail.com>
2026-09-18 18:54:56 +0200

    Refresh offline app automatically

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,13 @@
-const CACHE = "mylibrary-v7";
+const CACHE = "mylibrary-v8";
 const APP = ["./", "./index.html", "./manifest.webmanifest", "./icon-180.png", "./icon-512.png"];
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(APP)).then(() => self.skipWaiting())));
-self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
+self.addEventListener("activate", event => event.waitUntil(
+  caches.keys()
+    .then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
+    .then(() => self.clients.claim())
+    .then(() => self.clients.matchAll({type: "window"}))
+    .then(clients => clients.forEach(client => client.postMessage({type: "APP_UPDATED", version: CACHE})))
+));
 self.addEventListener("fetch", event => {
   if (event.request.method !== "GET") return;
   if (event.request.mode === "navigate") {
