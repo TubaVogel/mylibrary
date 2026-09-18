2d87f62707454d77f3441fc3d16a998e8abfff6d
TubaVogel <vogel.tuba@gmail.com>
2026-09-18 08:15:39 +0200

    Publish header colors and refresh cache

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "mylibrary-v5";
+const CACHE = "mylibrary-v6";
 const APP = ["./", "./index.html", "./manifest.webmanifest", "./icon-180.png", "./icon-512.png"];
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(APP)).then(() => self.skipWaiting())));
 self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
