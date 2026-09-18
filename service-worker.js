ed8842043e1eea9d4e3290bd31da990c8760b32a
TubaVogel <vogel.tuba@gmail.com>
2026-09-18 20:11:28 +0200

    Restore 149 verified book covers and refresh mobile cache

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE = "mylibrary-v10";
-const OFFLINE_PAGE = "./index.html?v=10";
+const CACHE = "mylibrary-v11";
+const OFFLINE_PAGE = "./index.html?v=11";
 const APP = [OFFLINE_PAGE, "./manifest.webmanifest", "./icon-180.png", "./icon-512.png"];
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(APP)).then(() => self.skipWaiting())));
 self.addEventListener("activate", event => event.waitUntil(
