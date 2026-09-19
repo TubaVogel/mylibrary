62c13a270f2e6f180fe439bda9b1a4b6147c8522
TubaVogel <vogel.tuba@gmail.com>
2026-09-19 20:12:37 +0200

    Fix cellar locations on existing devices
    Refreshes the offline cache and displays verified cellar books as Keller on devices with older saved data. No design changes.

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE = "mylibrary-v14";
-const OFFLINE_PAGE = "./index.html?v=14";
+const CACHE = "mylibrary-v15";
+const OFFLINE_PAGE = "./index.html?v=15";
 const APP = [OFFLINE_PAGE, "./manifest.webmanifest", "./icon-180.png", "./icon-512.png"];
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(APP)).then(() => self.skipWaiting())));
 self.addEventListener("activate", event => event.waitUntil(
